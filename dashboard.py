61de611fae3b524bb5d329f255cff5bae5924cb8
muhammaddfarhan <115083457+muhammaddfarhan@users.noreply.github.com>
2022-10-06 12:54:48 +0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -53,13 +53,13 @@ with st.container():
     st.write('\n')
     col2 , col3, col4 , col5 = st.columns(4)
     with col2:
-        st.write("<h5 style='text-align: left; color: Black;'>INQUIRY: 57</h5>", unsafe_allow_html=True)
+        st.write("<h5 style='text-align: left; color: Black;'>INQUIRY:  <span style='font-weight:normal'>57</span></h5>", unsafe_allow_html=True)
     with col3:
-        st.write("<h5 style='text-align: left; color: Black;'>COMPLAINTS: 8</h5>", unsafe_allow_html=True)
+        st.write("<h5 style='text-align: left; color: Black;'>COMPLAINTS:  <span style='font-weight:normal'>8</span></h5>", unsafe_allow_html=True)
     with col4:
-        st.write("<h5 style='text-align: left; color: Black;'>SUGGESTION: 0</h5>", unsafe_allow_html=True)
+        st.write("<h5 style='text-align: left; color: Black;'>SUGGESTION:  <span style='font-weight:normal'>0</span></h5>", unsafe_allow_html=True)
     with col5:
-        st.write("<h5 style='text-align: left; color: Black;'>COMPLIMENT: 0</h5>", unsafe_allow_html=True)
+        st.write("<h5 style='text-align: left; color: Black;'>COMPLIMENT:  <span style='font-weight:normal'>0</span></h5>", unsafe_allow_html=True)
 
 with st.container():
     st.write('\n')
@@ -122,4 +122,61 @@ with st.container():
         
 st.markdown("<h1 style='text-align: center; color: White;background-color:Black '>ONGOING SERVICES</h1>", unsafe_allow_html=True )
 st.write('\n')
-st.markdown("<h1 style='text-align: center; color: White;background-color:GREY '>CONNECT SERVICE</h1>", unsafe_allow_html=True )
+st.markdown("<h2 style='text-align: center; color: White;background-color:#094099 '>CONNECT SERVICE</h2>", unsafe_allow_html=True )
+
+with st.container():
+    st.write('\n')
+    col2 , col3= st.columns(2)
+    with col2:
+        st.write("<h5 style='text-align: left; color: Black;'>TEXT MESSAGE SENT: <span style='font-weight:normal'>356</span></h5>", unsafe_allow_html=True)
+    with col3:
+        st.write("<h5 style='text-align: right; color: Black;'>CUSTOMERS ENGAGED:  <span style='font-weight:normal'>178</span></h5>", unsafe_allow_html=True)
+   
+with st.container():
+    st.write('\n')
+    st.write('\n')
+    col2 , col3= st.columns(2)
+    with col2:
+        st.write("<h5 style='text-align: center; color: Black;'>CONNECT ACTIVATION (JULY - SEPTEMBER)</h5>", unsafe_allow_html=True)
+        st.write("<p style='text-align: center; color: Black;'>616</p>", unsafe_allow_html=True)
+    with col3:
+        st.write("<h5 style='text-align: center; color: Black;'>CONNECT ACTIVATION (SEPTEMBER)</h5>", unsafe_allow_html=True)
+        st.write("<p style='text-align: center; color: Black;'>178</p>", unsafe_allow_html=True)
+
+st.markdown("<h2 style='text-align: center; color: White;background-color:#094099 '>FIRST FREE SERVICE</h2>", unsafe_allow_html=True )
+
+with st.container():
+    st.write('\n')
+    col2 , col3= st.columns(2)
+    with col2:
+        st.write("<h5 style='text-align: left; color: Black;'>TEXT MESSAGE SENT: <span style='font-weight:normal'>356</span></h5>", unsafe_allow_html=True)
+    with col3:
+        st.write("<h5 style='text-align: right; color: Black;'>CUSTOMERS ENGAGED:  <span style='font-weight:normal'>178</span></h5>", unsafe_allow_html=True)
+
+with st.container():
+    st.write('\n')
+    col1 ,col2 , col3 = st.columns(3)
+    with col1:
+        st.write("<h5 style='text-align: CENTER; color: Black;'>AVAILED OR NOT</h5>", unsafe_allow_html=True)
+        st.write('\n\n\n')
+        chart_data = pd.DataFrame(
+        np.random.normal(5,1,2),
+        columns=["a"])
+    
+        st.bar_chart(chart_data)
+    with col2:
+        st.write("<h5 style='text-align: CENTER; color: Black;'>SERVICE EXPERIENCE</h5>", unsafe_allow_html=True)
+        st.write('\n\n\n')
+        chart_data = pd.DataFrame(
+        np.random.normal(5,1,10),
+        columns=["a"])
+    
+        st.bar_chart(chart_data)
+    with col3:
+        st.write("<h5 style='text-align: CENTER; color: Black;'>REASON FOR NOT AVAILING</h5>", unsafe_allow_html=True)
+        st.write('\n\n\n')
+        chart_data = pd.DataFrame(
+        np.random.normal(5,1,5),
+        columns=["a"])
+    
+        st.bar_chart(chart_data)      
